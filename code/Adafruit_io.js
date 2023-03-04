e160a89bca9da4e524c884abf37b940b92b92e7e
Akshay Arora <akshay575@yahoo.com>
2023-03-05 00:00:27 +0530

    Add Adafruit_io

---
+++ code/Adafruit_io.js
@@ -0,0 +1,9 @@
+const options = {
+  url: 'https://io.adafruit.com/api/v2/io_username/feeds',
+  headers: {
+    'X-AIO-Key': 'io_key_12345',
+    'Content-Type': 'application/json'
+  }
+};
+
+const apikey = aio_WCpzU40CAGsdLuslZSz4uhwxaiNj;
